5c8941b6fbec6ea0aa4bab8f939fd25bbe90265e
HJin_me <hjin.me@gmail.com>
2013-12-13 12:26:40 +0800

    request code

---
+++ config/constants.js
@@ -0,0 +1,15 @@
+/**
+ * Created by hjin on 13-12-13.
+ */
+
+module.exports = {
+  API_ENTRY: "https://openapi.baidu.com/rest/2.0",
+  BAE_APP_CONFIG: ".baeapp",
+  DEV_APP_CONFIG: ".devapp",
+  BAE_GLOBAL_CONFIG: ".bae",
+  REQUEST_CODE: {
+    client_id: 'DNbS8yG3IltI1Q1n3sogr41O',
+    response_type: 'device_code',
+    scope: 'basic'
+  }
+};
